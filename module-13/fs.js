4d655e51771cd8bf58a7dc1a2b80337911ef4b35
Imranhasanrimon <imranhasanrimon5@gmail.com>
2025-06-03 18:02:01 +0600

    making todo app

--- module-13/fs.js
+++ module-13/fs.js
@@ -13,8 +13,6 @@ const fs = require('node:fs');
 //Asyncronous way of FS
 console.log("task-1");
 const txt = "learning writeFile"
-// fs.writeFileSync('./module-13/hellow.txt', txt)
-// console.log("task-2");
 const data = fs.readFile("./module-13/hellow.txt", { encoding: "utf-8" }, (err, dataa) => {
     if (err) {
         console.log('sth went wrong', err);
--- module-13/logger-app/index.js
+++ module-13/logger-app/index.js
@@ -1,8 +1,22 @@
 const fs = require('fs')
+const pathModule = require('path')
 const arr = process.argv;
 const text = arr.slice(2).join(" ");
 
-fs.writeFile("./log.txt", text, { encoding: "utf-8" }, (err) => {
+if (!text) {
+    console.log("❌ No Text Added!");
+    console.log("Example: node index.js hellow world");
+    process.exit(1)
+}
+
+const time = new Date().toISOString();
+const message = `${text} ${time} \n`
+
+const path = pathModule.join(__dirname, 'log.txt')
+
+
+
+fs.appendFile(path, message, { encoding: "utf-8" }, (err) => {
     if (err) {
         console.log('Shit .....');
     }
