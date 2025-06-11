8006c37a7b7f9b3cb81b24addfe53cfb7aebcea8
Imranhasanrimon <imranhasanrimon5@gmail.com>
2025-06-11 18:32:52 +0600

    connecting DB

--- module-14/src/app/server.ts
+++ module-14/src/app/server.ts
@@ -1,9 +1,23 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
 import app from "./app";
 
 let server;
 const port = 3000
 
+const uri = "mongodb+srv://common:common@cluster0.7hbnv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
 const bootstrap = async () => {
+    await client.connect();
+    console.log('DB connected');
     server = app.listen(port, () => {
         console.log(`Example app listening on port ${port}`)
     })
