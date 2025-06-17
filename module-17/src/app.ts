19a2961f52086b24ba7413b725bce0d35de65aa7
Imranhasanrimon <imranhasanrimon5@gmail.com>
2025-06-17 22:59:26 +0600

    adding dev script

--- module-17/src/app.ts
+++ module-17/src/app.ts
@@ -1,4 +1,7 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 const app: Application = express();
 
+app.get("/", (req: Request, res: Response) => {
+    res.send({ message: "welcome to note app" })
+})
 export default app;
