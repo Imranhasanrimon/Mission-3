de9d9a032ba95e31158956689eda4fdfda1dd087
Imran Hasan <imranhasanrimon5@gmail.com>
2025-07-01 19:49:41 +0600

    learning commonjs module

---
+++ revision/m-12/file-1.js
@@ -0,0 +1,2 @@
+const { a } = require("./file-2");
+console.log(a);
---
+++ revision/m-12/file-2.js
@@ -0,0 +1,8 @@
+const a = 10;
+function add() {
+    return "nothing"
+}
+
+module.exports = { a, add };
+
+// console.log(module);
