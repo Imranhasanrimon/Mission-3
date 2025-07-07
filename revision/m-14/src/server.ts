8584f021ee7ab0ee5d9a253a4e4038c01299c52d
Imran Hasan <imranhasanrimon5@gmail.com>
2025-07-07 23:28:13 +0600

    conceptual session is over

---
+++ revision/m-14/src/server.ts
@@ -0,0 +1,11 @@
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("hellow world")
+})
+
+app.listen(3000, () => {
+    console.log("server is running");
+})
